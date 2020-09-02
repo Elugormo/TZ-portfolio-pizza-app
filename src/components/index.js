e2bfdc0e20efb2f406e62753c265e5e7cb2d7e64
Elugormo <romanomess@gmail.com>
2020-09-02 12:24:47 +0300

    finish commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as Header} from "./Header/Header";
-export { default as Button} from "./Button";
-export { default as Categories} from './Categories'
-export { default as SortPopup } from './SortPopup'
-export { default as Pizza } from './Pizza'
+export { default as Button } from './Button';
+export { default as Categories } from './Categories';
+export { default as PizzaBlock } from './PizzaBlock';
+export { default as PopupMenu } from './PopupMenu';
+export { default as SortPopup } from './SortPopup';
+export { default as CartItem } from './CartItem';
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,11 @@
-const { createStore } = require("redux");
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore();
+import rootReducer from './reducers';
+
+const composeEnhancers =
+  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
